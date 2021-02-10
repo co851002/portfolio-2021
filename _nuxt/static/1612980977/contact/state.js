8aed0a7ef7b02659a7633ba9ace22e1efabde013
co851002 <co851002@users.noreply.github.com>
2021-02-10 19:08:52 +0000

    deploy: 297f7d1d983e9f1dea7d2eeb3fe211a684543b10

--- _nuxt/static/1612980977/contact/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Fportfolio-2021\u002F_nuxt\u002Fstatic\u002F1612980977",layout:"default",error:null,state:{store:{}},serverRendered:a,routePath:"\u002Fcontact",config:{content:{dbHash:"717f7e11"}},globalRefs:{},colorMode:{preference:b,value:b,unknown:a,forced:false}}}(true,"system"));
---
+++ _nuxt/static/1612984129/projects/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Fportfolio-2021\u002F_nuxt\u002Fstatic\u002F1612984129",layout:"default",error:null,state:{store:{}},serverRendered:a,routePath:"\u002Fprojects",config:{content:{dbHash:"8fbe3582"}},globalRefs:{},colorMode:{preference:b,value:b,unknown:a,forced:false}}}(true,"system"));
