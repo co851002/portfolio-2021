5d74bbbc21bf01f03293dd3722d80b853b2335c6
Christo Oosthuizen <co851002@gmail.com>
2021-02-10 18:16:48 +0000

    27cfb78

---
+++ _nuxt/static/1612980977/blog/streetsmagazine/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Fportfolio-2021\u002F_nuxt\u002Fstatic\u002F1612980977",layout:"default",error:null,state:{store:{}},serverRendered:a,routePath:"\u002Fblog\u002Fstreetsmagazine",config:{content:{dbHash:"717f7e11"}},globalRefs:{},colorMode:{preference:b,value:b,unknown:a,forced:false}}}(true,"system"));
