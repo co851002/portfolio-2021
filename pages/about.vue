297f7d1d983e9f1dea7d2eeb3fe211a684543b10
Christo Oosthuizen <co851002@gmail.com>
2021-02-10 19:06:35 +0000

    page update

--- pages/about.vue
+++ pages/about.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="flex flex-col align-middle justify-center z-10">
         <h1 class="text-4xl font-bold ">
-          ABout me <br />
-          I'm a fullstack developer <br />based in London, UK
+          About me
         </h1>
   </div>
 
