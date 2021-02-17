6be17bdd10966d17a15f85fca5c15bab04462394
co851002 <co851002@users.noreply.github.com>
2021-02-17 23:14:34 +0000

    deploy: 9e1c5bd3bbd8eb1268509dca7ded69fe75899687

--- _nuxt/static/1613255373/contact/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Fportfolio-2021\u002F_nuxt\u002Fstatic\u002F1613255373",layout:"default",error:null,state:{store:{}},serverRendered:a,routePath:"\u002Fcontact",config:{content:{dbHash:"854c2be2"}},globalRefs:{},colorMode:{preference:b,value:b,unknown:a,forced:false}}}(true,"system"));
