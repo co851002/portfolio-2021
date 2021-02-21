4d47890596be68826cd54f6d93dc2f04b463b667
co851002 <co851002@users.noreply.github.com>
2021-02-21 23:42:13 +0000

    deploy: 9c881ebac3434d69ffad1796f38e9e3e0dc5efb4

---
+++ _nuxt/080816a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{413:function(t,e,n){"use strict";n.r(e);var l={props:{title:{type:String}},render:function(t){return t("div",{class:"flex justify-center font-title text-bold text-center text-3xl pt-4 pb-10",id:"heading-1"},this.title)}},d=n(11),component=Object(d.a)(l,undefined,undefined,!1,null,null,null);e.default=component.exports}}]);
---
+++ _nuxt/static/1613950929/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Fcontact","\u002Fexperience","\u002Fprojects","\u002Frecents","\u002Fskills","\u002Fblog\u002Fstreetsmagazine","\u002Fblog\u002Ftag\u002Fwordpress","\u002Fblog\u002Ftag\u002Fjavascript","\u002Fblog\u002Ftag\u002Fvuejs","\u002Fblog\u002Ftag\u002Faws","\u002Fblog\u002Fbrandtech","\u002Fblog\u002Fmobkoiwebsite"]})
